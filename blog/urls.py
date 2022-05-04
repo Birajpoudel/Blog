65d90ffb7cd0502bf049cf54eda46ebd85e66238
Biraj Poudel <birajpoudel23@gmail.com>
2022-05-04 18:27:14 +0545

    Template inheritance

---
+++ blog/urls.py
@@ -0,0 +1,12 @@
+
+from django.contrib import admin
+from django.urls import path
+from .import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('base/',views.BASE,name='base'),
+    path('',views.INDEX,name='home')
+
+
+]
